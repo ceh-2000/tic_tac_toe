b767770ad8911c9cffa0db1ff360ea1495febce1
Clare Heinbaugh <clare.heinbaugh@gmail.com>
2020-02-08 14:24:51 -0500

    Initial Commit

---
+++ Session1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Session1
+//
+//  Created by Clare Heinbaugh on 2/8/20.
+//  Copyright © 2020 Clare Heinbaugh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
